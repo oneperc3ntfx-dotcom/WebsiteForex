54fb710740a855b40815fd2edf0d3b92ad8c5a8f
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-05-06 23:08:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-from database import check_code
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# ================= LOGIN =================
+VALID_CODES = ["VIP123"]
+
 @app.get("/", response_class=HTMLResponse)
 async def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
 @app.post("/")
 async def login(request: Request, code: str = Form(...)):
-    if check_code(code):
+    if code in VALID_CODES:
         response = RedirectResponse("/dashboard", status_code=303)
-        response.set_cookie("access", code)
+        response.set_cookie(key="access", value=code)
         return response
     return templates.TemplateResponse("login.html", {
         "request": request,
         "error": "Kode salah"
     })
 
-# ================= DASHBOARD =================
 @app.get("/dashboard", response_class=HTMLResponse)
 async def dashboard(request: Request):
     if not request.cookies.get("access"):
         return RedirectResponse("/")
     return templates.TemplateResponse("dashboard.html", {"request": request})
 
-# ================= PAIR =================
 @app.get("/pair/{name}", response_class=HTMLResponse)
 async def pair(request: Request, name: str):
     if not request.cookies.get("access"):
